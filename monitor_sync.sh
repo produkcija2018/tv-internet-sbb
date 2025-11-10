9ea50e72f51e85c267b777c26688d8240fb2f38e
produkcija2018 <produkcija2018@gmail.com>
2025-11-10 13:29:28 +0100

    Termux sync: 2025-11-10 13:29:27

--- monitor_sync.sh
+++ monitor_sync.sh
@@ -1,1 +1,8 @@
-404: Not Found
+#!/bin/bash
+echo "🔍 MONITORING TERMUX"
+cd ~/moje-zabele-ke-
+git pull origin main
+git add .
+git commit -m "Termux sync: $(date '+%Y-%m-%d %H:%M:%S')"
+git push origin main
+echo "✅ ZAVRŠENO"
